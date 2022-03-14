83263d184ff6b176a7d666cf85c2e5ad83a96ffe
cmoraes5 <moraescaio0502@gmail.com>
2022-03-14 19:05:59 -0300

    first commit

---
+++ scripts/game.js
@@ -0,0 +1,78 @@
+// A REGRA DO NEGÓCIO
+// * Arquivo responsável apenas pela lógica do jogos
+// * Tudo que não tem a ver com a interface gtráfica
+
+// => Iniciar minhas variáveis
+// => Board representa cada um dos quadrados, podendo ter 3 estados: X, O e Vazio
+// => PlayerTime diz de quem é a vez, sendo 0 para o player 1 e 1 par o player 2
+// - 25 => Verificar se há vencedor
+
+// * existe somente 8 opções de vitória
+
+// todo ==> Mostrar o nome de quem ganhou 
+// todo ==> Mostrar o movimento
+
+let board = ['','','','','','','','',''];
+let playerTime = 0; 
+let gameOver = false;
+
+let symbols = ['o', 'x'];
+
+
+let winStates = [
+
+    // Horizontal - ways to win
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+
+    // Vertical - ways to win 
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+
+    // Diagonal - ways to win
+    [0,4,8],
+    [6,4,2],
+]
+function handleMove(position){
+    
+    if(gameOver){
+        return;
+    }
+
+        if (board[position] == ''){
+            board[position] = symbols[playerTime];
+
+            gameOver = isWin();
+
+        if(gameOver == false){
+        
+        // * Verificar se playerTime é = 0, se for Vdd, colocar 1 no pT, se ñ, colocar 0
+            playerTime = (playerTime == 0) ? 1 : 0; // condição ternária
+        }
+    }
+
+    return gameOver;
+}
+
+function isWin() {
+
+
+    for(let i =0; i<winStates.length; i++){
+        let seq = winStates[i];
+
+        let pos1 = seq[0];
+        let pos2 = seq[1];
+        let pos3 = seq[2];
+
+        if(board[pos1] == board[pos2] &&
+            board[pos1] == board[pos3] &&
+            board[pos1] != ''){
+                
+                return true;
+            }
+    }
+
+    return false;
+}
---
+++ scripts/interface.js
@@ -0,0 +1,48 @@
+// * Arquivo responsável por fazer o "meio de campo" entre
+// * o HTML e a parte lógica do jogo
+
+// todo => Criar evento nos quadrados, se algm clicar em um qdrdo, quero saber qual foi
+
+document.addEventListener('DOMContentLoaded', ()=>{
+
+    let squares =  document.querySelectorAll(".square");
+
+    squares.forEach((square)=>{
+        square.addEventListener('click', handleClick);
+    })
+
+});
+
+
+function handleClick(event){
+
+    console.log(event.target);
+    console.log(board);
+
+    let square = event.target;
+    let position = square.id;
+
+    if(handleMove(position)){
+        if(playerTime == 0){
+            playerTime = 1;
+        }
+        else{
+            playerTime = 2;
+        }
+
+        setTimeout(() => {
+            alert("O Jogo acabou - O vencedor foi o player " + playerTime)
+        }, 11);
+    };
+    updateSquare(position);
+}
+
+function updateSquare(position){
+    let square = document.getElementById(position.toString());
+    let symbol = board[position];
+    square.innerHTML = `<div class='${symbol}'></div>`
+}
+
+function reset(){
+    window.location.reload();
+}
